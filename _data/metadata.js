a2444f85ce76567db6441ec24dcc6b4ef7c722f7
Ricky de Laveaga <git@rdela.com>
2023-06-16 15:10:54 -0700

    upd meta desc

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,8 @@ module.exports = {
 	title: "Spatial Movies",
 	url: "https://spatialmovi.es",
 	language: "en",
-	description: "Spatial Movies website",
+	description:
+		"Spatial Movies is a new studio dedicated to creating spatial audiovisual media and traditional media about it.",
 	author: {
 		name: "Ricky de Laveaga",
 		email: "ricky@rdela.com",
